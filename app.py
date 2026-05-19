ec3f7f5b2b3767092b0a2d35d68fdaa0427e8e81
이석현 <leshmov0316@gmail.com>
2026-05-19 13:23:10 +0900

    feat:  wordcloud test run

--- app.py
+++ app.py
@@ -1,6 +1,31 @@
 import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+from wordcloud import WordCloud
 
-st.set_page_config(page_title="Streamlit App TEST", page_icon=":guardsman:", layout="wide")
+st.title("전처리 이전 워드클라우드 테스트")
 
-st.title("Streamlit App TEST")
-st.write("This is a test Streamlit app.")
+files = {
+    "유퀴즈": "./data/yuquiz.csv",
+    "런닝맨": "./data/runningman.csv",
+    "놀면뭐하니": "./data/nolmyun.csv"
+}
+
+program = st.selectbox("프로그램 선택", list(files.keys()))
+
+df = pd.read_csv(files[program])
+
+text = " ".join(df["comment"].dropna().astype(str))
+
+wc = WordCloud(
+    font_path="C:/Windows/Fonts/malgun.ttf",
+    width=1000,
+    height=500,
+    background_color="white"
+).generate(text)
+
+fig, ax = plt.subplots(figsize=(12, 6))
+ax.imshow(wc)
+ax.axis("off")
+
+st.pyplot(fig)
